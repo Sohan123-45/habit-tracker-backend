2a4e5cada8a39d53ce0259aaf63fc94494e2cb06
Sohan123-45 <sohan.dintakurthi@gmail.com>
2026-03-31 21:12:46 +0530

    added publickey and urlendpoint

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,7 +1,9 @@
 const {ImageKit}=require("@imagekit/nodejs");
 
 const ImageKitClient=new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey:process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 })
 
 async function uploadFile(file,mimetype){
