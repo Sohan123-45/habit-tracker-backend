6391472d43a03300dfef4bae975546e5f66cb0a7
Sohan123-45 <sohan.dintakurthi@gmail.com>
2026-04-01 19:10:03 +0530

    changes in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,10 +2,17 @@ const jwt=require("jsonwebtoken");
 const userModel=require("../models/user.model");
 
 async function isAuthenticated(req,res,next){
-    const token=req.cookies.token;
-    if(!token){
+    const authHeader = req.headers.authorization;   
+    if(!authHeader){
         return res.status(401).json({message:"Unauthorized"});
     }
+
+    // Format: "Bearer <token>"
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Invalid token format" });
+    }
+    
     try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
         req.user={
